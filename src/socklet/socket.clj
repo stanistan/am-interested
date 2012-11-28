265d5aff140e7540328f18f164a4ef5208621665
Zachary Allaun <zach.allaun@gmail.com>
2012-11-28 14:56:00 -0500

    Small wrapper around Java sockets.

---
+++ src/socklet/socket.clj
@@ -0,0 +1,61 @@
+(ns socklet.socket
+  (:refer-clojure :exclude [read])
+  (:import [java.net ServerSocket Socket]))
+
+(defn create-server
+  [port]
+  (ServerSocket. port))
+
+(defn close-socket
+  [socket]
+  (.close socket))
+
+(defn accept
+  [server]
+  (.accept server))
+
+(defn create-socket
+  [host port]
+  (Socket. host port))
+
+(defn get-streams
+  "Returns a [input output] stream pair."
+  [socket]
+  [(.getInputStream socket) (.getOutputStream socket)])
+
+(defn read-available
+  [input-stream]
+  (.available input-stream))
+
+(defn write
+  "Writes a string to an output stream."
+  [stream string]
+  (.write stream (.getBytes string)))
+
+(defn read
+  "Reads a byte from an input stream."
+  [stream]
+  (.read stream))
+
+(defn read-byte-stream
+  "Reads a stream of bytes into a byte array until no more
+  bytes are available to be read."
+  [input]
+  (loop [bytes []]
+    (if (not= 0 (read-available input))
+      (let [b (read input)]
+        (recur (conj bytes b)))
+      (if (empty? bytes)
+        (recur bytes)
+        bytes))))
+
+(defn read-and-handle-byte-stream
+  [input f]
+  (f (read-byte-stream input)))
+
+(defn listen
+  [input f]
+  (future
+    (loop []
+      (read-and-handle-byte-stream input f)
+      (recur))))
