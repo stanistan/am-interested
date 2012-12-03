3475460975bfeefc8932be7a8c552c8ca0c3b0e7
Zachary Allaun <zach.allaun@gmail.com>
2012-12-03 15:40:45 -0500

    Create cast relation utilities.

---
+++ test/chomp/cast_test.clj
@@ -0,0 +1,29 @@
+(ns chomp.cast-test
+  (:use chomp.cast midje.sweet))
+
+(fact "about simple cast-to"
+      (defcast String Long
+        :forward (fn [s] (read-string s))
+        :backward (fn [i] (str i)))
+
+      (cast-to String 1) => "1"
+      (cast-to String "1") => "1"
+      (cast-to Long "1") => 1
+      (cast-to Long 1) => 1)
+
+(fact "about record cast-to"
+      (defrecord Foo [x])
+
+      (defcast Foo String
+        :forward (fn [f] (:x f))
+        :backward (fn [s] (->Foo s)))
+
+      (cast-to Foo "foo") => (->Foo "foo")
+      (cast-to Foo (->Foo "foo")) => (->Foo "foo")
+      (cast-to String (->Foo "foo")) => "foo"
+      (cast-to String "foo") => "foo")
+
+;; Midje cannot capture compile-time exceptions thrown from macros.
+;; All expansion happens at the same time.
+;; (fact "about cast assertion"
+;;       (defcast String Long) => (throws))
